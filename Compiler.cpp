ce8cc4f15ed74b5fdd6b462ddfca018cf51c0d0b
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-05-22 00:07:20 -0400

    Update Compiler.cpp

--- Compiler.cpp
+++ Compiler.cpp
@@ -368,28 +368,6 @@ public:
     }
 };
 
-section .text
-global _start
-
-_start:
-    call myFunc
-    mov rax, 60
-    xor rdi, rdi
-    syscall
-
-myFunc:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 32              ; allocate local frame
-
-    ; [function body emitted here]
-
-.return:
-    mov rsp, rbp
-    pop rbp
-    ret
-
-
 //--------------------------------------------------
 // --- MAIN: FULL COMPILER TEST HARNESS ---
 //--------------------------------------------------
